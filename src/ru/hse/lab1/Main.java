2354acca7069240de9653a5611d81474c13cd507
elizavetakrupina <azil89670@gmail.com>
2026-02-08 16:42:20 +0300

    2 и 3 задание

---
+++ src/ru/hse/lab1/Main.java
@@ -0,0 +1,14 @@
+package ru.hse.lab1;
+import ru.hse.lab1.second.Person;
+/**
+ * @author Крупина Елизавета Денисовна
+ * @version 1.0
+ * @since 2026
+ */
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Иван", 20);
+        System.out.println("Имя: " + person.name);
+        System.out.println("Возраст: " + person.age);
+    }
+}
